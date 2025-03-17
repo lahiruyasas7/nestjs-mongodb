063247d90493841e0eb9a0ab5582f8836be64452
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-17 11:11:11 +0530

    created user create api

---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "src/schemas/User.schema";
+import { CreateUserDto } from "./dto/CreateUser.dto";
+
+@Injectable()
+export class UserService {
+constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+){
+
+}
+
+creteUser(creteUserDto : CreateUserDto){
+    const newUser = new this.userModel(creteUserDto);
+    return newUser.save();
+}
+}
