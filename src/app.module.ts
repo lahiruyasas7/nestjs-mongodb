fa123a3c7a0f5d8332720c36b532d8bbd333b865
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-19 23:06:41 +0530

    added PostModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest_mongo_tutorial'),
     UserModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
